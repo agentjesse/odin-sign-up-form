7bf180bd460282fd2a5e521c22c1b85328d15512
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-20 00:35:30 -0500

    Final code before simplification of validation Fn

--- script.js
+++ script.js
@@ -3,18 +3,10 @@ const passwordElement = document.querySelector(`input[name='password']`);
 const confirmPasswordElement = document.querySelector(`input[name='confirmPassword']`);
 let pass1, pass2;
 
-document.querySelector('form section').addEventListener('input',e=> {
-  e.stopPropagation();
-  const elem = e.target;
-  switch (elem.name) {
-    case 'password':
-      pass1 = elem.value;
-      // console.log(pass1, pass2);
-      break;
-    case 'confirmPassword':    
-      pass2 = elem.value;
-      // console.log(pass1, pass2);
-  }
+const checkPasswordsAndSetStyles = (pass1,pass2)=>{
+  //handle when pass1 is an empty string leading to wrongly adding errors:
+  pass1 = pass1 === '' ? undefined : pass1;
+  pass2 = pass2 === '' ? undefined : pass2;
   if (pass1 !== pass2 ){
     badPasswordMsgElement.classList.remove('hideBadPassMsg'); //remove class to show badPasswordMsgElement
     passwordElement.classList.add('error'); //enable class to border red
@@ -24,4 +16,17 @@ document.querySelector('form section').addEventListener('input',e=> {
     confirmPasswordElement.classList.remove('error'); //remove class to clear red border
     passwordElement.classList.remove('error'); 
   }
+}
+
+document.querySelector('form section').addEventListener('input',e=> {
+  e.stopPropagation();
+  const elem = e.target;
+  switch (elem.name) {
+    case 'password':
+      pass1 = elem.value;
+      break;
+    case 'confirmPassword':    
+      pass2 = elem.value;
+  }
+  checkPasswordsAndSetStyles(pass1,pass2);
 });
