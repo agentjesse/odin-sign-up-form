ad70890887b2757968d74f151db22cb1aa0a452d
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-19 23:23:20 -0500

    Added password checking
    js for checking values, and css updated to ignore red and green validity for password fields

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+const badPasswordMsgElement = document.querySelector('.badPasswordMsg');
+const passwordElement = document.querySelector(`input[name='password']`);
+const confirmPasswordElement = document.querySelector(`input[name='confirmPassword']`);
+let pass1, pass2;
 
-document.querySelector('form section').addEventListener('input',e=>{
+document.querySelector('form section').addEventListener('input',e=> {
   e.stopPropagation();
-  if (e.target.name === 'password' || e.target.name === 'confirmPassword'){
-    console.log(e.target.name);
+  const elem = e.target;
+  switch (elem.name) {
+    case 'password':
+      pass1 = elem.value;
+      // console.log(pass1, pass2);
+      break;
+    case 'confirmPassword':    
+      pass2 = elem.value;
+      // console.log(pass1, pass2);
   }
-})
+  if (pass1 !== pass2 ){
+    badPasswordMsgElement.classList.remove('hideBadPassMsg'); //remove class to show badPasswordMsgElement
+    passwordElement.classList.add('error'); //enable class to border red
+    confirmPasswordElement.classList.add('error');
+  } else if (pass1 === pass2 ){
+    badPasswordMsgElement.classList.add('hideBadPassMsg');//add class to hide basPasswordMsgElement
+    confirmPasswordElement.classList.remove('error'); //remove class to clear red border
+    passwordElement.classList.remove('error'); 
+  }
+});
